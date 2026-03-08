5f6c17a0ddb6a3e3627d207f5e3d0fd823cd2a48
Tom Kazimiers <tom@voodoo-arts.net>
2026-03-08 21:09:57 +0100

    Config: move app to root base path (/)

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/app/", // Set base path for deployment under /acme context
+	base: "/", // Set base path for deployment under /acme context
 });
