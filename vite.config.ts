88592693b985a8822c3048bcee6877c2d5cefa4c
Tom Kazimiers <tom@voodoo-arts.net>
2026-03-12 11:33:43 +0100

    For consistency, use an empty string as base path in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/", // Set base path for deployment under /acme context
+	base: "", // Set base path for deployment under /acme context
 });
